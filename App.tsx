cfe019e19279dae3ec01cf109e2c7eae085d1353
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2021-06-21 17:23:59 -0300

    Componente  #3

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/signin';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World, NLW Toggether</Text>
-      <StatusBar style="auto" />
-    </View>
+  return(
+    <SignIn />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
